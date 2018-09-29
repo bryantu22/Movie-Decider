e4f596d2fa30827950a7f3a96b9b495c3961bc01
Bryan Tu <bryantu22@gmail.com>
2018-09-29 01:26:44 -0500

    Bryan (#9)
    * first commit to own branch
    
    * 100px test
    
    * tester
    
    * add images + description
    
    * added spacing to text
    
    * Created 'View Recommendation' (Not based off of actor, will do once algorithm is done)
    
    * fixed merge issue
    
    * working merge
    
    * UI update

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -1,14 +1,23 @@
 import React from "react";
 import axios from "axios";
 import Card from "./card.js";
+import Modal from "react-responsive-modal";
 
 class MovieRow extends React.Component {
   constructor(props) {
     super();
 
-    this.state = { showCard: false, data: "a" };
+    this.state = { showCard: false, data: "", open: false };
   }
 
+  onOpenModal = () => {
+    this.changeCard();
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
   viewMovie = () => {
     const url = "https://www.themoviedb.org/movie/" + this.props.movie.id;
     window.location.href = url;
@@ -20,7 +29,7 @@ class MovieRow extends React.Component {
       .get(
         "https://api.themoviedb.org/3/movie/" +
           this.props.movie.id +
-          "/credits?api_key=886cbad81f9f93405487e46a949d9eec"
+          "/credits?api_key=cfe422613b250f702980a3bbf9e90716"
       )
       .then(response => {
         console.log(response.data.cast);
@@ -29,7 +38,7 @@ class MovieRow extends React.Component {
 
   changeCard = () => {
     this.setState({ showCard: !this.state.showCard });
-    var ID = "apple";
+    var ID = "";
     axios
       .get(
         "https://api.themoviedb.org/3/movie/" +
@@ -57,28 +66,30 @@ class MovieRow extends React.Component {
   render() {
     return (
       <div>
-        <img
-          alt="poster"
-          width="10%"
-          src={this.props.movie.poster_src}
-          style={{ display: "inline-block" }}
-        />
+
         <div
           style={{
             display: "inline-block",
-            width: "80%",
-            marginBottom: "10px",
-            verticalAlign: "top",
-            paddingLeft: "2%"
+            marginRight: "10px"
           }}
         >
-          <h3>{this.props.movie.title}</h3>
-          <p>{this.props.movie.overview}</p>
-          {/*<button onClick={this.viewMovie}>Click Me</button>*/}
-          <button onClick={this.viewActors}>View Actors</button>
-          <button onClick={this.changeCard}>View Recommendation</button>
+          <img
+            alt="poster"
+            src={this.props.movie.poster_src}
+            onClick={this.onOpenModal}
+          />
+          <div>
+            {/*<button onClick={this.viewMovie}>Click Me</button>*/}
+            {/* <button onClick={this.viewActors}>View Actors</button> */}
+            {/* <button onClick={this.changeCard}>View Recommendation</button> */}
+          </div>
         </div>
-        {this.state.showCard ? <Card data={this.state.data} /> : ""}
+        <div>
+          <Modal open={this.state.open} onClose={this.onCloseModal}>
+            <Card data={this.state.data} />
+          </Modal>
+
+        </div>
       </div>
     );
   }
---
+++ src/nav.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import video from "./video-camera.png";
+
+class Nav extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="nav">
+        <img className="nav-img" alt="poster" src={video} height="45" />
+        <span className="nav-span">Movie Decider</span>
+      </div>
+    );
+  }
+}
+
+export default Nav;
